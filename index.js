ae1a4c4b95dbaa30b2321421765e34a2b993bd43
BertheCILLY <87968222+BertheCILLY@users.noreply.github.com>
2021-10-19 10:42:59 +0200

    js
    création

---
+++ index.js
@@ -0,0 +1,40 @@
+
+fetch('http://localhost:3000/api/teddies', {
+
+}).then(function(response) {
+    return response.json();
+    
+}).then(function(data){
+    for (let product of data) {
+        displayOneProduct(product);
+    }
+}).catch(err => {
+    console.log('err', err);
+    alert('Serveur non disponible');
+});
+
+//Fonction qui crée les bloc HTML
+
+function displayOneProduct(product) {
+    let newElement = document.createElement('a');
+    let element = document.getElementById('ours-section');
+
+    newElement.classList.add('ours-details');
+    newElement.classList.add('text-center');
+    newElement.setAttribute('href', `produit.html?id=${product._id}`);
+    element.appendChild(newElement);
+
+    let img = document.createElement('img');
+    img.src = product.imageUrl;
+    img.classList.add('ours-img');
+    newElement.appendChild(img);
+
+    let h2 = document.createElement('h2');
+    h2.innerText = product.name;
+    newElement.appendChild(h2);
+
+     let p = document.createElement('p');
+    p.innerText = 'Prix : ' +  product.price.toLocaleString('fr-FR') + '€';
+    newElement.appendChild(p);
+
+}
