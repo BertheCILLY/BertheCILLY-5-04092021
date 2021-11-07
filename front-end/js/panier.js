235b24e574c0ddabc1ab6f6ce64ff80cad792493
CILLY  berthe <87968222+BertheCILLY@users.noreply.github.com>
2021-11-07 13:39:17 +0100

    front-end
    encore

---
+++ front-end/js/panier.js
@@ -0,0 +1,236 @@
+
+
+
+function displayCart() {
+    let test = document.querySelector(".width-to-empty-cart");
+    let cartCard = document.querySelector(".cart-card");
+    let emptyCart = document.querySelector(".if-empty-cart");
+    
+        console.log(displayCart);
+
+        
+        //Récupération des données du localStorage
+
+    let panier = JSON.parse(localStorage.getItem('products'));
+    let cart = document.querySelector(".contenu-panier");
+
+         consol.log(panier);
+
+        // Si le tableau copié du localStorage contient au moins un objet, on affiche le panier 
+    if (localStorage.getItem("products")) {
+         cartCard.style.display = "flex";
+        cartCard.style.flexDirection = "column";
+        cartCard.style.justifyContent = "space-around";
+         emptyCart.style.display = "none";
+
+    } else {
+            //message d'erreur si soucis d'affichage
+            console.log('Cette adresse ne correspond pas a la page demandée');
+        
+     console.log(products);
+
+
+
+
+        for (let produit in panier) {
+           let productRow = document.createElement("div");
+            cart.insertBefore(productRow, test);
+            productRow.classList.add("text-center", "panier-produit");
+
+            let productName = document.createElement("div");
+            productRow.appendChild(productName);
+            productName.classList.add("panier-title");
+            productName.innerHTML = panier[produit].name;
+
+          let productPrice = document.createElement("div");
+          productRow.appendChild(productPrice);
+          productPrice.classList.add(
+                "panier-title",
+                 "data-price",
+                "price"
+                
+         );
+         console.log(productPrice);
+    
+
+
+
+
+   /*pourquoi ca ne fonctionne pa s????
+
+//fonction de création du contenu de la page
+
+function createLignePanier(index) {
+    let newElementTeddy = document.createElement('div');
+    let elementTeddy = document.getElementById('panier-produit');
+
+    newElementTeddy.classList.add('panier-detail');
+    newElementTeddy.classList.add('text-center');
+    elementTeddy.appendChild(newElementTeddy);
+
+    //création du contenu du produit panier
+
+    let teddyName = document.createElement('p');
+    teddyName.innerText = index.name;
+    newElementTeddy.appendChild(teddyName);
+
+    let teddyImg = document.createElement('img');
+    teddyImg.src = index.img;
+    teddyImg.classList.add('teddy-img');
+    newElementTeddy.appendChild(teddyImg);
+
+    //Ajoute l'option de couleur choisie
+
+    let newElementOption = document.createElement('div');
+    let elementOption = document.getElementById('panier-option');
+    elementOption.appendChild(newElementOption);
+
+    newElementOption.classList.add('panier-color');
+
+    let teddyColor = document.createElement('p');
+    teddyColor.innerText = index.color ? index.color : 'Aucune option';
+    
+    newElementOption.appendChild(teddyColor);
+
+    //Ajoute le prix
+
+    let newElementPrice = document.createElement('div');
+    let elementPrice = document.getElementById('panier-prix');
+    elementPrice.appendChild(newElementPrice);
+
+    newElementPrice.classList.add('panier-price');
+
+    let price = document.createElement('p');
+    price.innerText = index.price.toLocaleString('fr-FR') + ' €';
+    newElementPrice.appendChild(price);
+
+    //Ajoute le bouton supprimer
+
+    let newElementButton = document.createElement('div');
+    let elementButton = document.getElementById('panier-bouton');
+    elementButton.appendChild(newElementButton);
+
+    newElementButton.classList.add('panier-button');
+
+    let button = document.createElement('input');
+    button.id = 'bouton-supprimer';
+    button.type = 'button';
+    button.value = 'Supprimer'
+    newElementButton.appendChild(button);
+
+    function deleteLigne() {
+        
+        let panier = JSON.parse(localStorage.getItem('panier'));
+        panier.splice(index,1);
+        localStorage.setItem('panier', JSON.stringify(panier));
+        
+        newElementPrice.remove();
+        newElementOption.remove();
+        newElementTeddy.remove();
+        newElementButton.remove();
+
+        totalPrice(panier);
+    }
+
+    button.addEventListener('click',deleteLigne);
+}
+
+//fonction qui met la première lettre en majuscule
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+// fonction qui calcule le prix total et l'insère dans la page
+
+function totalPrice(panier) {
+    
+    let totalPrice = 0;
+    for (let i=0; i<panier.length; i++) {
+        totalPrice = totalPrice + panier[i].price;
+    }
+
+    let elementTotalPrice = document.getElementById('contenu-total-price');
+
+    elementTotalPrice.innerText = 'Le montant total de la commande est : ' + totalPrice.toLocaleString('fr-FR') + '€.';
+
+    localStorage.setItem("totalPriceConfirmation", totalPrice);
+}
+
+//fonction de validation du formulaire et envoie à l'API
+
+function  validation(event) {
+    event.preventDefault();
+    let allValid = true;
+    for (i=0; i < listeId.length; i++) {
+    
+        let verif = document.getElementById(listeId[i]);
+        let missVerif = document.getElementById('miss' + capitalizeFirstLetter(listeId[i]));
+        let nameValid = /^[a-zA-Zéèîï][a-zéèêàçîï]+([-'\s][a-zA-Zéèîï][a-zéèêàçîï]+)?$/;
+        let adressValid = /^([0-9]+)+[\s][a-zA-Zéèîï][a-zéèêàçîï]+([-'\s][a-zA-Zéèîï][a-zéèêàçîï]+)?$/;
+        let mailValid = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+        
+        //si le champs est vide
+
+        if(verif.validity.valueMissing){
+            event.preventDefault();
+            missVerif.textContent = 'Champ manquant';
+            missVerif.style.color = 'red';
+            allValid = false;
+
+        // si le format de données n'est pas correct 
+
+        } else if ((listeId[i] == 'nom' || listeId[i] == 'prenom' || listeId[i] == 'ville') && nameValid.test(verif.value) == false) {
+            event.preventDefault();
+            missVerif.textContent = 'Format incorrect';
+            missVerif.style.color = 'red';
+            allValid = false;
+        } else if (listeId[i] == 'adresse' && adressValid.test(verif.value) == false) {
+            event.preventDefault();
+            missVerif.textContent = 'Format incorrect';
+            missVerif.style.color = 'red';
+            allValid = false;
+        } else if (listeId[i] == 'email' && mailValid.test(verif.value) == false) {
+            event.preventDefault();
+            missVerif.textContent = 'Format incorrect';
+            missVerif.style.color = 'red';
+            allValid = false;
+        };
+    }
+
+    if (allValid == true) {
+            
+        let contactForm = {
+            lastName : document.getElementById(listeId[0]).value,
+            firstName : document.getElementById(listeId[1]).value,
+            email : document.getElementById(listeId[2]).value,
+            address : document.getElementById(listeId[3]).value,
+            city : document.getElementById(listeId[4]).value, 
+        }
+
+        let produitPost = [];
+        for (let i=0;i<panier.length; i++) {
+            produitPost.push(panier[i].id)
+        }
+
+        let objetPost = {
+            contact : contactForm,
+            products : produitPost
+        }
+
+        fetch('http://localhost:3000/api/teddies/order', {
+           
+            body: JSON.stringify(objetPost)
+        }).then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            localStorage.setItem('order', JSON.stringify(data));
+            window.location.href = './confirmation.html';
+        }).catch(err => {
+   
+        }
+      
+  
+    }
+}   
+    */
--- front-end/js/produit.js
+++ front-end/js/produit.js
@@ -9,55 +9,98 @@ const id = urlParams.get('id');
 
 fetch('http://localhost:3000/api/teddies/'+id)
 
-.then(function(response) {
-    return response.json();
+        .then(function(response) {
+             return response.json();
   
-}).then(function(data){
- console.log(data);
-//Création de la carte avec l'image , le titre la description et le prix en euro
-document.getElementById("image").src = data.imageUrl;
-document.getElementById("titre").textContent= data.name;
-document.getElementById("description").textContent= data.description;
-document.getElementById("prix").textContent= data.price + ' €';
-console.log(data.colors)
+    }).then(function(data)
+    
+ {
 
-//  Création des choix de couleur à chaque passage sur la boucle
+   
+  //Création de la carte avec l'image , le titre la description et le prix en euro
+    document.getElementById("image").src = data.imageUrl;
+    document.getElementById("titre").textContent= data.name;
+    document.getElementById("description").textContent= data.description;
+    document.getElementById("prix").textContent= data.price + ' €';
+   
 
-let option = document.createElement('option');
-for (let i=0; i < data.colors.length; i++) {
-    console.log(data.colors[i])
-    option = document.createElement('option');
-option.setAttribute('value', data.colors[i]);
-option.textContent = data.colors[i];
-document.getElementById("color-select").appendChild(option);
+        //  Création des choix de couleur à chaque passage sur la boucle
 
-console.log(data.colors);
-}
-// Sélection de l'id du produit 
-const idform = document.querySelector("#bearNum");
+        let option = document.createElement('option');
+        for (let i=0; i < data.colors.length; i++) {
+       
+        option = document.createElement('option');
+        option.setAttribute('value', data.colors[i]);
+        option.textContent = data.colors[i];
+        document.getElementById("color-select").appendChild(option);
 
+     
+        }
 
+       
+      
 
-let newElement = document.createElement('div');
-let element = document.getElementById('ours-section');
+           // function addToCart() {
+            const addToCartBtn = document.querySelector(".add-to-cart");
+            const confirmation = document.querySelector(".added-to-cart-confirmation");
+             const textConfirmation = document.querySelector(".confirmation-text");
+    
 
-console.log(newElement);
-
-newElement.classList.add('ours-detail');
-newElement.classList.add('text-center');
-element.appendChild(newElement);
-
-teddyRecap(data, newElement);
-
- //création du bouton panier
-
- let button = document.createElement('button');
- button.innerText = 'Ajouter au panier';
- button.classList.add('ours-button');
- newElement.appendChild(button);
-
- console.log(button)
+            
+            addToCartBtn.addEventListener("click", () => {
 
 
- 
-});
+                   // ------ Création du produit qui sera ajouté au panier
+                let productAdded = {
+                  name: data.name,
+                  price: data.price,
+                   _id: id,
+                  color:document.getElementById("color-select").value,
+
+                 
+                }; 
+        
+              
+                      // Si le localStorage existe, on récupère son contenu, on l'insère dans le tableau arrayProductsInCart, puis on le renvoit vers le localStorage avec le nouveau produit ajouté.
+                let arrayProductInCart ;
+                arrayProductsInCart = localStorage.getItem("products");
+                          //On regarde si on a une variable "products"dans notre storage
+                  if (arrayProductsInCart === null ){
+
+                  arrayProductsInCart = [] ; //si le panier est null tu me crée un tableau vide
+                  }
+                      else{
+
+                          //Sinon dans "arrayProducts" on reprend ce qui exist et on fait un JsonParse pour recuperer notre tableau de produit
+                      arrayProductsInCart = JSON.parse(localStorage.getItem("products"));
+                              
+                  }
+
+                         /*
+                       // Fenêtre pop Pup
+                       const popPupConfirmation = () => {
+
+                            if (window.confim (`${data.name} option: ${color-select} à bien ètè ajouté au panier, consultez le panier OK ou revenir à l'accueil ANNULER`)){
+                              window.location.href = "panier.html";
+                            }else{
+                              window.location.href = "index.html";
+                            }
+                            }
+                            
+                                */
+                  
+                     arrayProductsInCart.push(productAdded);
+                    localStorage.setItem("products", JSON.stringify(arrayProductsInCart));
+                    //On ajoute notre produit a notre tableau et qu'on remet notre tableau en storage (uniquement en format "sting")
+            
+         
+           
+            
+          
+            })
+
+    }); 
+  
+
+
+
